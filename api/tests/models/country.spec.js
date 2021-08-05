33886585adcaf081c37d6611633f36b1b84fb6f8
Lucero Amaolo <lamaolo.m@gmail.com>
2021-08-05 14:46:52 -0300

    Agregados tests para modelo de Country

--- api/tests/models/country.spec.js
+++ api/tests/models/country.spec.js
@@ -1,21 +1,68 @@
-const { Country, conn } = require('../../src/db.js');
-const { expect } = require('chai');
+const { Country, conn } = require("../../src/db.js");
 
-describe('Country model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    }));
-  describe('Validators', () => {
-    beforeEach(() => Country.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Country.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
+describe("Country model", () => {
+  before(() => {
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    });
+  });
+  describe("Validators", () => {
+    beforeEach(async () => {
+      await Country.sync({ force: true });
+    });
+    describe("Required fields", () => {
+      it("Should throw an error if a required field is null", (done) => {
+        Country.create({
+          id: "ARG",
+          name: "Argentina",
+          subregion: "South America",
+        })
+          .then(() => {
+            done(
+              "Deberia lanzar un error si no se rellenan todos los campos requeridos"
+            );
+          })
+          .catch(() => {
+            done();
+          });
       });
-      it('should work when its a valid name', () => {
-        Country.create({ name: 'Argentina' });
+
+      it("Should create a row when all required fields are passed", (done) => {
+        Country.create({
+          id: "ARG",
+          name: "Argentina",
+          image: "https://restcountries.eu/data/arg.svg",
+          continent: "Americas",
+          capital: "Buenos Aires",
+          subregion: "South America",
+          area: 2780400,
+          population: 43590400,
+        })
+          .then(() => {
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+
+      it("ID must be a three characters string", (done) => {
+        Country.create({
+          id: "ARGENTINA",
+          name: "Argentina",
+          image: "https://restcountries.eu/data/arg.svg",
+          continent: "Americas",
+          capital: "Buenos Aires",
+          subregion: "South America",
+          area: 2780400,
+          population: 43590400,
+        })
+          .then(() => {
+            done("ID must be a three characters string");
+          })
+          .catch(() => {
+            done();
+          });
       });
     });
   });
--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -1,24 +1,58 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require("chai");
-const session = require("supertest-session");
-const app = require("../../src/app.js");
-const { Country, conn } = require("../../src/db.js");
+// /* eslint-disable import/no-extraneous-dependencies */
+// const { expect } = require("chai");
+// const session = require("supertest-session");
+// const app = require("../../src/app.js");
+// const { Country, conn } = require("../../src/db.js");
 
-const agent = session(app);
-const country = {
-  name: "Argentina",
-};
+// const agent = session(app);
 
-describe("Country routes", () => {
-  before(() =>
-    conn.authenticate().catch((err) => {
-      console.error("Unable to connect to the database:", err);
-    })
-  );
-  beforeEach(() =>
-    Country.sync({ force: true }).then(() => Country.create(country))
-  );
-  describe("GET /countries", () => {
-    it("should get 200", () => agent.get("/api/countries").expect(200));
-  });
-});
+// const countries = [
+//   {
+//     id: "ARG",
+//     name: "Argentina",
+//     image: "https://restcountries.eu/data/arg.svg",
+//     continent: "Americas",
+//     capital: "Buenos Aires",
+//     subregion: "South America",
+//     area: 2780400,
+//     population: 43590400,
+//     createdAt: "2021-08-05T01:07:57.413Z",
+//     updatedAt: "2021-08-05T01:07:57.413Z",
+//     activities: [],
+//   },
+//   {
+//     id: "ESP",
+//     name: "Spain",
+//     image: "https://restcountries.eu/data/esp.svg",
+//     continent: "Europe",
+//     capital: "Madrid",
+//     subregion: "Southern Europe",
+//     area: 505992,
+//     population: 46438422,
+//     createdAt: "2021-08-05T01:07:57.413Z",
+//     updatedAt: "2021-08-05T01:07:57.413Z",
+//     activities: [],
+//   },
+// ];
+
+// describe("Test countries routes", () => {
+//   before(() =>
+//     conn.authenticate().catch((err) => {
+//       console.error("Unable to connect to the database:", err);
+//     })
+//   );
+
+//   describe("GET /api/countries", () => {
+//     Country.sync({ force: true }).then(() => {
+//       Country.bulkCreate(countries).then(() => {
+//         it("should get 200", () => agent.get("/api/countries").expect(200));
+
+//         it("responds with the list of countries", () => {
+//           agent.get("/api/countries").then((res) => {
+//             expect(res.body.data).to.be.equal(countries);
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
